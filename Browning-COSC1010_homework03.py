ee98fc6668fe86f143273da1e1eb7d601f3cc91c
GRANIT <gbrowni1@uwyo.edu>
2024-11-04 06:09:27 +0000

    foundations in place?

---
+++ Browning-COSC1010_homework03.py
@@ -0,0 +1,22 @@
+# Grant Browning
+# UWYO COSC 1010
+# Submission Date: 11/05/24
+# HW 03
+# Lab Section: Austin
+# Sources, people worked with, help given to:
+while True:
+    date_input = input("Please enter the date you would like to know the day of the week for in MM/DD/YYYY format:")
+
+    def day_o_week_calc(MM, DD, YYYY):
+         
+
+        days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+        days_in_year = 365
+
+        date_str = date_input.split("/")
+    break
+
+
+print(date_str)
+#Example output: 02/21/2022 Monday, or 02/29/2023 Invalid Date
